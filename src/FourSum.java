4fc973053eddb2cf66ef795442cfbb12d854f386
Niroigen Suntharam <niroigen@gmail.com>
2018-10-01 14:44:38 -0400

    1.4.14 4-sum. Develop an algorithm for the 4-sum problem.

---
+++ src/FourSum.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FourSum {
+    public static void main(String[] args) {
+        ArrayList<Integer> values = new ArrayList<>(Arrays.asList(1,4,5,8,24,9,2,6,3));
+        fourSumProblem(values,20);
+    }
+
+    private static void fourSumProblem(ArrayList<Integer> arrayList, int sum) {
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            for (int j = i + 1; j < arrayList.size() - 1; j++) {
+                for(int k = j + 1; k < arrayList.size() - 1; k++) {
+                    for (int l = k + 1; l < arrayList.size(); l++) {
+                        if (arrayList.get(i) + arrayList.get(j) + arrayList.get(k) + arrayList.get(l) == sum) {
+                            System.out.println("Found values " + arrayList.get(i) + " " + arrayList.get(j) + " " + arrayList.get(k) + " " + arrayList.get(l));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
