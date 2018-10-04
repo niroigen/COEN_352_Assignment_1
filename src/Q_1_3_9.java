8c409f2c2eb791dab90786b6652482d3cceaf791
Niroigen Suntharam <niroigen@gmail.com>
2018-10-03 21:08:02 -0400

    Adding command line arguments got Q 1.3.9

--- src/Q_1_3_9.java
+++ src/Q_1_3_9.java
@@ -1,21 +1,8 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Stack;
-
 public class Q_1_3_9 {
-
-	/*
-	 * 1.3.9 Write a program that takes from standard input an expression without left parentheses
-		and prints the equivalent infix expression with the parentheses inserted. For
-		example, given the input:
-		1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )
-		your program should print
-		( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )
-	 */
-	
 	public static void main(String[] args) {
-		 String s = "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
+		 String s = args[0];
 	        Stack<Character> stack = new Stack<Character>();
 	        for (int i = 0; i < s.length(); i++)
 	        {
@@ -28,40 +15,32 @@ public class Q_1_3_9 {
 	}
 	
 	private static String fixInfix(Stack<Character> stack) {
-		// Example 1 + 2 ) * 5 + 4 )
-		// keep taking in characters until you reach a )
-		// once that happens bracket them together
-		// Known stuff ( 1 + 2 )
-		// You will keep reading and then find the next time this happens
-		// Now you will know what you had before  and add a left bracket infront of that
-		// ( ( 1 + 2 ) * 5 )
-		
 		Stack<Character> tempStack = new Stack<Character>();
 		ArrayList<String> numbers = new ArrayList<String>();
 		
 		for (int i = 0; i < stack.size(); i++) {
 			if (i == stack.size() - 1 && numbers.size() > 1) {
-				String answer = "(";
+				StringBuilder answer = new StringBuilder("(");
 				for (String number : numbers) {
-					answer += number;
+					answer.append(number);
 				}
-				answer += ")";
-				return answer;
+				answer.append(")");
+				return answer.toString();
 			} else if (stack.elementAt(i) == ')') {
-				String closedBracket = "(";
+				StringBuilder closedBracket = new StringBuilder("(");
 								
 				for (Character tempCharacter : tempStack) {
-					closedBracket += tempCharacter;
+					closedBracket.append(tempCharacter);
 				}
 				
-				closedBracket += ")";
+				closedBracket.append(")");
 				
 				if (i < stack.size() - 2) {
 					i++;
-					closedBracket += stack.elementAt(i);
+					closedBracket.append(stack.elementAt(i));
 				}
 				
-				numbers.add(closedBracket);
+				numbers.add(closedBracket.toString());
 				
 				tempStack.clear();
 			} else {
@@ -69,12 +48,12 @@ public class Q_1_3_9 {
 			}
 		}
 		
-		String output = "";
+		StringBuilder output = new StringBuilder();
 		
 		for (String number : numbers) {
-			output += number;
+			output.append(number);
 		}
 		
-		return output;
+		return output.toString();
 	}
 }
